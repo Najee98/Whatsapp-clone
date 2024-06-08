27c9475812d54832f74d0348f2f6a011e891251a
Najee <najee.shaheen.98@gmail.com>
2024-06-08 16:00:55 +0300

    web socket update

--- src/main/java/com/whatsapp/Whatsappclone/Controllers/RealTimeChat.java
+++ src/main/java/com/whatsapp/Whatsappclone/Controllers/RealTimeChat.java
@@ -1,23 +1,32 @@
-package com.whatsapp.Whatsappclone.Controllers;
-
 import com.whatsapp.Whatsappclone.Models.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @RequiredArgsConstructor
 public class RealTimeChat {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/message")
-    @SendTo("/goup/public")
-    public Message receiveMessage(@Payload Message message){
+    @SendTo("/group/public")
+    public Message receiveMessage(@Payload Message message) {
+        try {
+            // Process the received message
 
-        simpMessagingTemplate.convertAndSend("/goup/" + message.getChat().getId().toString(), message);
+            // Broadcast the message to all clients in the same group
+            simpMessagingTemplate.convertAndSend("/group/" + message.getChat().getId().toString(), message);
 
-        return message;
+            return message;
+        } catch (Exception e) {
+            // Handle exceptions
+            // You can log the exception or send a custom error message to the client
+            e.printStackTrace();
+            return null; // Or return an error message object
+        }
     }
 }
