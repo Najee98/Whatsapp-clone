7078eefdaec2376402aad2f4511d87fc9ffdaffc
Najee <najee.shaheen.98@gmail.com>
2024-06-10 21:12:02 +0300

    store every message sent in the database

--- src/main/java/com/whatsapp/Whatsappclone/Controllers/RealTimeChatController.java
+++ src/main/java/com/whatsapp/Whatsappclone/Controllers/RealTimeChatController.java
@@ -2,11 +2,17 @@ package com.whatsapp.Whatsappclone.Controllers;
 
 
 import com.whatsapp.Whatsappclone.Dto.SendMessageRequest;
+import com.whatsapp.Whatsappclone.Models.AppUser;
+import com.whatsapp.Whatsappclone.Models.Message;
+import com.whatsapp.Whatsappclone.Services.MessageService;
+import com.whatsapp.Whatsappclone.Services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -15,10 +21,19 @@ import org.springframework.stereotype.Controller;
 public class RealTimeChatController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private final MessageService messageService;
+    private final UserService userService;
 
     @MessageMapping("/chat")
     public void chat(@Payload SendMessageRequest message) {
         log.info("Message received: {}", message);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        AppUser user = userService.findUserByUsername(username);
+
+        Message msg = messageService.sendMessage(message, user);
 
         String topic = "/topic/chat/" + message.getChatId();
         simpMessagingTemplate.convertAndSend(topic, message);
